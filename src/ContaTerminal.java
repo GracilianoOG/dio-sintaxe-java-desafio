44361abc0b50d05e198e28ee2c26e2413cee21d9
Gabriel <gabriel.gracilianomb@gmail.com>
2024-08-04 15:53:14 -0300

    Close scanner after its use

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -21,5 +21,7 @@ public class ContaTerminal {
         saldo = scanner.nextDouble();
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque");
+
+        scanner.close();
     }
 }
