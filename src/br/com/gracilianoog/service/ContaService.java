884dd1ac831e6d1ffcebd8c96b7b7d6f386e56d4
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-18 21:27:52 -0300

    Create ContaService

---
+++ src/br/com/gracilianoog/service/ContaService.java
@@ -0,0 +1,32 @@
+package br.com.gracilianoog.service;
+
+import br.com.gracilianoog.DAO.ContaDAO;
+import br.com.gracilianoog.model.Conta;
+
+public class ContaService {
+    private final ContaDAO contaDAO = new ContaDAO();
+
+    public void depositar(int numero, double valorDeposito) {
+        Conta conta = contaDAO.buscarContaPorNumero(numero);
+
+        if(conta == null) {
+            throw new IllegalArgumentException("A conta com o número " + numero + " não existe!");
+        }
+
+        conta.setSaldo(conta.getSaldo() + valorDeposito);
+    }
+
+    public void sacar(int numero, double valorSaque) {
+        Conta conta = contaDAO.buscarContaPorNumero(numero);
+
+        if(conta == null) {
+            throw new IllegalArgumentException("A conta com o número " + numero + " não existe!");
+        }
+
+        if(valorSaque > conta.getSaldo()) {
+            throw new IllegalArgumentException("O saldo deve ser menor ou igual ao saldo disponível!");
+        }
+
+        conta.setSaldo(conta.getSaldo() - valorSaque);
+    }
+}
