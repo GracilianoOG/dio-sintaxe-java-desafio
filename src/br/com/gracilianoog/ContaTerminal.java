28e5116d42eb22e088fbb836b470535cf535a3ca
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-18 22:31:07 -0300

    Create menu to manage operations
    Method to create account created.

--- src/br/com/gracilianoog/ContaTerminal.java
+++ src/br/com/gracilianoog/ContaTerminal.java
@@ -1,29 +1,61 @@
 package br.com.gracilianoog;
 
+import br.com.gracilianoog.model.Cliente;
+import br.com.gracilianoog.service.ClienteService;
+import br.com.gracilianoog.service.ContaService;
+
 import java.util.Scanner;
 
 public class ContaTerminal {
+    private static Scanner scanner = new Scanner(System.in);
+    private static ContaService contaService = new ContaService();
+    private static ClienteService clienteService = new ClienteService();
+
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        int opcao = -1;
+
+        while(true) {
+            System.out.println("=============== Banco Pão ===============");
+            System.out.println("1 - Cadastrar nova conta");
+            System.out.println("2 - Cadastrar novo cliente");
+            System.out.println("3 - Depositar valor");
+            System.out.println("4 - Sacar valor");
+            System.out.println("5 - Sair");
+            System.out.print("--> ");
+
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1 -> {
+                    cadastrarNovaConta();
+                }
+            }
+        }
+    }
+
+    public static void cadastrarNovaConta() {
         int numero;
         String agencia;
-        String nomeCliente;
         double saldo;
+        Cliente cliente;
+        String nomeCliente;
 
-        System.out.println("Por favor, digite o nome do cliente: ");
-        nomeCliente = scanner.nextLine();
-
-        System.out.println("Por favor, digite a agência: ");
-        agencia = scanner.next();
-
-        System.out.println("Por favor, digite o número da conta: ");
+        System.out.println("Informe o número da conta: ");
         numero = scanner.nextInt();
 
-        System.out.println("Por favor, digite o saldo da conta: ");
+        System.out.println("Informe a agência: ");
+        agencia = scanner.next();
+
+        System.out.println("Informe o saldo: ");
         saldo = scanner.nextDouble();
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$ " + saldo + " já está disponível para saque");
+        System.out.println("Informe o nome do cliente: ");
+        nomeCliente = scanner.next();
 
-        scanner.close();
+        cliente = clienteService.buscarClientePorNome(nomeCliente);
+
+        contaService.cadastrarConta(numero, agencia, saldo, cliente);
+
+        System.out.println("Cliente cadastrado: " + cliente.getNome());
     }
 }
