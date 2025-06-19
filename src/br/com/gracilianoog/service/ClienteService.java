9fb3f2f03e28ba358a102b5e3768a19a0eba1a76
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-18 22:03:52 -0300

    Create ClienteService

---
+++ src/br/com/gracilianoog/service/ClienteService.java
@@ -0,0 +1,14 @@
+package br.com.gracilianoog.service;
+
+import br.com.gracilianoog.DAO.ClienteDAO;
+import br.com.gracilianoog.model.Cliente;
+
+public class ClienteService {
+    private final ClienteDAO clienteDAO = new ClienteDAO();
+
+    public Cliente cadastrarCliente(String nome) {
+        Cliente cliente = new Cliente(nome);
+        clienteDAO.salvar(cliente);
+        return cliente;
+    }
+}
